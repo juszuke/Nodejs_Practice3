469e4f4a4d40a144972f6ab3876062a3066e3373
juszuke <yusuke.ino.yusuke@gmail.com>
2020-12-31 00:49:04 +0900

    ESLintとPrettierを導入

--- quiz/app.js
+++ quiz/app.js
@@ -1,53 +1,54 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const fetch = require('node-fetch');
+/* eslint-disable no-undef */
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const fetch = require("node-fetch");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+app.get("/", function (req, res) {
+  res.render("index", { title: "Express" });
 });
-app.get('/api/v1/quizzes', (req, res) => {
-  let quizList = [];
+app.get("/api/v1/quizzes", (req, res) => {
+  const quizList = [];
   // クイズデータをTriviaAPIで取得
-  fetch('https://opentdb.com/api.php?amount=10')
+  fetch("https://opentdb.com/api.php?amount=10")
     .then((response) => response.json())
-    .then(data => {
-      for(let i = 0; i < data.results.length; i++) {
+    .then((data) => {
+      for (let i = 0; i < data.results.length; i++) {
         quizList.push(data.results[i]);
       }
       console.log(quizList);
     })
-    .then(() => res.json(quizList))
+    .then(() => res.json(quizList));
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- quiz/public/javascripts/main.js
+++ quiz/public/javascripts/main.js
@@ -1,15 +1,15 @@
-'use strict'
-        
+"use strict";
+
 // 各オブジェクトを取得する
-const title = document.getElementById('title');
-const category = document.getElementById('category');
-const difficulty = document.getElementById('difficulty');
-const question = document.getElementById('question');
+const title = document.getElementById("title");
+const category = document.getElementById("category");
+const difficulty = document.getElementById("difficulty");
+const question = document.getElementById("question");
 
 // ボタン
-const start = document.getElementById('start');
-const answers = document.getElementById('answers');
-const home = document.getElementById('home');
+const start = document.getElementById("start");
+const answers = document.getElementById("answers");
+const home = document.getElementById("home");
 
 let quizSet = [];
 let currentNum = 0;
@@ -21,39 +21,40 @@ const initQuizList = () => {
   quizSet = [];
   currentNum = 0;
   score = 0;
-}
+};
 
 // 初期化＋ホーム画面へ移動する処理
+// eslint-disable-next-line no-unused-vars
 const showHome = () => {
-  title.textContent = 'ようこそ';
-  category.textContent = '';
-  difficulty.textContent = '';
-  question.textContent = '以下のボタンをクリック';
-  home.classList.add('none');
-  start.classList.remove('none');
+  title.textContent = "ようこそ";
+  category.textContent = "";
+  difficulty.textContent = "";
+  question.textContent = "以下のボタンをクリック";
+  home.classList.add("none");
+  start.classList.remove("none");
 
   initQuizList();
-}
+};
 
 // クイズデータを外部APIから取得する処理
 const fetchQuizList = () => {
-  return fetch('/api/v1/quizzes')
-    .then(response => response.json())
-    .then(data => {
-      for(let i = 0; i < data.length; i++) {
+  return fetch("/api/v1/quizzes")
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
         quizSet.push(data[i]);
       }
-    })
-}
+    });
+};
 
 // クイズデータ取得＋待機画面へ移動する処理
 const showLoadingPage = () => {
-  title.textContent = '取得中';
-  category.textContent = '';
-  difficulty.textContent = '';
-  question.textContent = '少々お待ちください';
-  start.classList.add('none');
-}
+  title.textContent = "取得中";
+  category.textContent = "";
+  difficulty.textContent = "";
+  question.textContent = "少々お待ちください";
+  start.classList.add("none");
+};
 
 // 選択肢をシャッフルする処理
 function shuffleChoices(choices) {
@@ -71,10 +72,10 @@ function checkAnswer(button) {
   }
   isAnswered = true;
   if (button.textContent === quizSet[currentNum].correct_answer) {
-    console.log('correct');
+    console.log("correct");
     score++;
   } else {
-    console.log('wrong'); 
+    console.log("wrong");
   }
 }
 
@@ -86,41 +87,44 @@ const setQuiz = () => {
   category.textContent = `[ジャンル] ${quizSet[currentNum].category}`;
   difficulty.textContent = `[難易度] ${quizSet[currentNum].difficulty}`;
   question.textContent = quizSet[currentNum].question;
-  
+
   // correct_answerとincorrect_answersを結合してchoicesを作成する
-  const choices = quizSet[currentNum].incorrect_answers.concat(quizSet[currentNum].correct_answer);
+  const choices = quizSet[currentNum].incorrect_answers.concat(
+    quizSet[currentNum].correct_answer
+  );
   quizSet[currentNum]["choices"] = choices; // choicesをquizSetに追加する
-  
-  while(answers.firstChild) {
+
+  while (answers.firstChild) {
     answers.removeChild(answers.firstChild);
   }
-  
+
   const shuffledChoices = shuffleChoices(quizSet[currentNum].choices);
-  shuffledChoices.forEach(choice => {
-    const button = document.createElement('button');
+  shuffledChoices.forEach((choice) => {
+    const button = document.createElement("button");
     button.textContent = choice;
-    button.addEventListener('click', () => {
+    button.addEventListener("click", () => {
       checkAnswer(button);
     });
     answers.appendChild(button);
   });
-  answers.classList.remove('none');
-}
+  answers.classList.remove("none");
+};
 
 // 結果画面へ移動する処理
 const showResult = () => {
-  while(answers.firstChild) {
+  while (answers.firstChild) {
     answers.removeChild(answers.firstChild);
   }
-  
-  title.textContent = `あなたの正解数は${score}です！！`;
-  category.textContent = '';
-  difficulty.textContent = '';
-  question.textContent = '再度チャレンジしたい場合は以下をクリック！!';
-  answers.classList.add('none');
-  home.classList.remove('none');
-}
 
+  title.textContent = `あなたの正解数は${score}です！！`;
+  category.textContent = "";
+  difficulty.textContent = "";
+  question.textContent = "再度チャレンジしたい場合は以下をクリック！!";
+  answers.classList.add("none");
+  home.classList.remove("none");
+};
+
+// eslint-disable-next-line no-unused-vars
 async function loadQuizList() {
   showLoadingPage();
   await fetchQuizList();
@@ -128,7 +132,7 @@ async function loadQuizList() {
 }
 
 // 最後の問題に回答したら結果ページへ、それ以外は次の問題へ進む処理
-answers.addEventListener('click', () => {
+answers.addEventListener("click", () => {
   if (currentNum === quizSet.length - 1) {
     showResult();
   } else {
